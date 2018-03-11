9cbfc023c2fbf615eeb68fb8a108e231c8df665c
vadrevua <vadrevua@vcu.edu>
2018-03-11 03:52:23 -0400

    still working on PA3 for NLP

--- NLP/Programming Assingments/3/tagger.pl
+++ NLP/Programming Assingments/3/tagger.pl
@@ -5,10 +5,11 @@
 use strict;
 use warnings;
 # /"(\[)|(" ")|(\])"/
-my @wordsArr;
+use Data::Dumper qw(Dumper);
+my @trainArr;
+my @testArr;
 my $readingString;
-my %hash;
-my %totalhash;
+my %trainingHash;
 my $train;
 my $test;
 
@@ -16,49 +17,74 @@ if($#ARGV == 1){
   $train = $ARGV[0];
   $test = $ARGV[1];
   if(((substr $train, -4) eq "\.txt") && ((substr $test, -4) eq "\.txt")){
-    }
-  else{
-    exit;
-    }
   }
   else{
     exit;
+  }
+}
+else{
+  exit;
+}
+
+my $filename = $train;
+open(my $fh, '<:encoding(UTF-8)', $filename)
+or die "Could not open file '$filename' $!";
+
+while (my $row = <$fh>) {
+  chomp $row;
+  my @words = split /\]|\[|\s+/, $row;
+  foreach my $val (@words){
+    if($val eq ""){
+      next;
     }
-  my $filename = $train;
-  open(my $fh, '<:encoding(UTF-8)', $filename)
-  or die "Could not open file '$filename' $!";
-
-  while (my $row = <$fh>) {
-
-    chomp $row;
-    my @words = split /\]|\[|\s+/, $row;
-    foreach my $val (@words){
-      if($val eq ""){
-        next;
-      }
-      else{
-        push(@wordsArr, $val);
-      }
+    else{
+      push(@trainArr, $val);
     }
   }
+}
 
-foreach my $wordTag (@wordsArr){
-my $word;
-my $tag;
-my $slashWord;
-my $secWord;
+foreach my $wordTag (@trainArr){
+  my $word;
+  my $tag;
+  my $slashWord;
+  my $secWord;
   if($wordTag =~ m/\\\//){
     ($word, $slashWord) = split/\\/, $wordTag;
     my $update = substr $slashWord, 1;
     ($secWord, $tag) = split/\//,$update;
     # print "$secWord\n"
     $word = "$word/$secWord";
-    }
+  }
   else{
     ($word, $tag) = split/\//, $wordTag;
-    }
+  }
 
-    %hash = ($tag,$word);
-  # print "$tag\n";
+  if($trainingHash{$word}{$tag}){
+    $trainingHash{$word}{$tag}++;
+  }
+  else{
+    $trainingHash{$word}{$tag} = 1;
+  }
 }
-print %hash;
+# print Dumper \%trainingHash;
+
+
+$filename = $test;
+open($fh, '<:encoding(UTF-8)', $filename)
+or die "Could not open file '$filename' $!";
+
+while (my $row = <$fh>) {
+  chomp $row;
+  my @testwords = split /\]|\[|\s+/, $row;
+  foreach my $val (@testwords){
+    if($val eq ""){
+      next;
+    }
+    else{
+      push(@testArr, $val);
+    }
+  }
+}
+foreach my $woe (@testArr) {
+  print "$woe\n";
+}
